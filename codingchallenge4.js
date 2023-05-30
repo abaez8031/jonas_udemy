213286973721360d341fba2c3d7bf6688844d298
abaez8031 <abaez8031@gmail.com>
2023-05-30 00:36:55 -0400

    codingchallenge4

---
+++ codingchallenge4.js
@@ -0,0 +1,9 @@
+let bill = 275
+let tip = bill >= 50 && bill <= 300 ? .15 * bill : .2 * bill
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value was ${bill + tip}`)
+bill = 40;
+tip = bill >= 50 && bill <= 300 ? .15 * bill : .2 * bill
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value was ${bill + tip}`)
+bill = 430;
+tip = bill >= 50 && bill <= 300 ? .15 * bill : .2 * bill
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value was ${bill + tip}`)
