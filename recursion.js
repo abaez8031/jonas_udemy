65c43fdb4068d7f05904606516d0069600e28551
abaez8031 <abaez8031@gmail.com>
2023-06-11 22:46:38 -0400

    recursion

--- recursion.js
+++ recursion.js
@@ -3,4 +3,11 @@ const factorial = function(n) {
   return factorial(n - 1) * n
 }
 
-console.log(factorial(5))
+
+const fib = function(n) {
+  if(n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(5))
+console.log(fib(7))
