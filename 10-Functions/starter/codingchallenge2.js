1089bf3043d127a6f16ec433e45828e2da3d1416
abaez8031 <abaez8031@gmail.com>
2023-06-15 16:10:02 -0400

    codingchallenge2

--- 10-Functions/starter/codingchallenge2.js
+++ 10-Functions/starter/codingchallenge2.js
@@ -1,4 +1,7 @@
 (function () {
   const header = document.querySelector('h1');
   header.style.color = "red"
+  document.body.addEventListener("click", () => {
+    header.style.color = "blue"
+  })
 })();
